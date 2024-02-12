ae35e7ba89f7d13e8ec6d40d757cf4f6c465155f
Nancy <nancy.nardelli@gmail.com>
2024-02-12 16:00:24 -0300

    Creación Capas

---
+++ C-50285-Nardelli-Nancy-Web-Api/DTOs/ProductoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace C_50285_Nardelli_Nancy_Web_Api.DTOs
+{
+    public class ProductoDTO
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal? Costo { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Stock { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
